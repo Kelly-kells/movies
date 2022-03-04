6d7e9fc26748acb550eb09170565396c1adb4f2a
unknown <kellskelechi@gmail.com>
2022-03-04 22:20:07 +0100

    landing-page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react'
+import { useEffect } from 'react';
 
-export default App;
+
+   const API_URL = 'https://www.omdbapi.com?apikey=6fac29e';
+
+   const App = () => {
+
+
+    const searchMovies = async (title) =>{
+       
+        const response=await fetch (`${API_URL}&s=${title}`)
+        const data= await response.json();
+        console.log(data.Search);
+    }
+    useEffect(()=>{
+    searchMovies('Batman')
+    },[])
+    
+    return (
+      <div>
+    
+      </div>
+    )
+  }
+  
+  
+
+export default App
