f5f9602f6ab1e03b8dfd2536578a1431971a9ff5
unknown <kellskelechi@gmail.com>
2022-03-07 01:45:54 +0100

    api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 
 import React from 'react'
 import { useEffect } from 'react';
+import searchIcon from './search.svg'
 
 
    const API_URL = 'https://www.omdbapi.com?apikey=6fac29e';
 
    const App = () => {
 
+
+    const movie1={
+        
+            "Title": "Batman: The Killing Joke",
+            "Year": "2016",
+            "imdbID": "tt4853102",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMTdjZTliODYtNWExMi00NjQ1LWIzN2MtN2Q5NTg5NTk3NzliL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
+        
+    }
 
     const searchMovies = async (title) =>{
        
@@ -19,8 +30,20 @@ import { useEffect } from 'react';
     },[])
     
     return (
-      <div>
-    
+      <div className='app'>
+    <h1>FILMORE</h1>
+    <div className="search">
+        <input placeholder='search for movies' />
+        <img src={searchIcon} alt="search" onClick={()=>{}}/>
+        </div>
+        <div className="container">
+            <div className="movie">
+                <p>{movie1.Year}</p>
+                <img src={movie1.Poster} alt={movie1.Title} />
+                <div><span>{movie1.Type}</span></div>
+                </div>
+                
+            </div>
       </div>
     )
   }
